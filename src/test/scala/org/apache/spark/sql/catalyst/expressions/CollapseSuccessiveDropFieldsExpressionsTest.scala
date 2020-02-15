af83bad9d69af2b3d5cdd074b76b7073a118dbac
fqaiser94@gmail.com <fqaiser94@gmail.com>
2020-02-15 17:06:59 -0500

    added check for value as well

--- src/test/scala/org/apache/spark/sql/catalyst/expressions/CollapseSuccessiveDropFieldsExpressionsTest.scala
+++ src/test/scala/org/apache/spark/sql/catalyst/expressions/CollapseSuccessiveDropFieldsExpressionsTest.scala
@@ -7,7 +7,6 @@ import org.apache.spark.sql.catalyst.plans.logical.{LogicalPlan, OneRowRelation,
 import org.apache.spark.sql.catalyst.rules.RuleExecutor
 import org.apache.spark.sql.types.{IntegerType, StructField, StructType}
 
-
 class CollapseSuccessiveDropFieldsExpressionsTest extends PlanTest with ExpressionEvalHelper {
 
   private val spark = SparkSession.builder().appName("spark-test").master("local").getOrCreate()
@@ -15,51 +14,52 @@ class CollapseSuccessiveDropFieldsExpressionsTest extends PlanTest with Expressi
   private val sparkContext = spark.sparkContext
   sparkContext.setLogLevel("ERROR")
 
-  //  private val df: DataFrame = spark.createDataFrame(sparkContext.parallelize(
-  //    Row(Row(1, 2, 3, 4)) :: Nil),
-  //    StructType(Seq(
-  //      StructField("a", StructType(Seq(
-  //        StructField("a", IntegerType),
-  //        StructField("b", IntegerType),
-  //        StructField("c", IntegerType)))))))
-  //
-  //  test("drop rule") {
-  //    val result: DataFrame = df.withColumn("a", $"a".dropFields("a").dropFields("b"))
-  //
-  //    result.explain
-  //    result.printSchema()
-  //    result.show(false)
-  //  }
-
-  object Optimize extends RuleExecutor[LogicalPlan] {
+  private object Optimize extends RuleExecutor[LogicalPlan] {
     val batches: Seq[Optimize.Batch] = Batch(
       "CollapseDropFieldsExpressionsTest",
       FixedPoint(50),
       CollapseSuccessiveDropFieldsExpressions) :: Nil
   }
 
-  protected def assertEquivalent(e1: Expression, e2: Expression): Unit = {
+  protected def assertEquivalentPlanAndEvaluation(e1: Expression, e2: Expression, value: Any): Unit = {
     val correctAnswer = Project(Alias(e2, "out")() :: Nil, OneRowRelation()).analyze
     val actual = Optimize.execute(Project(Alias(e1, "out")() :: Nil, OneRowRelation()).analyze)
     comparePlans(actual, correctAnswer)
+    checkEvaluation(e1, value)
+    checkEvaluation(e2, value)
   }
 
   private val inputStruct = {
     val schema = StructType(Seq(
       StructField("a", IntegerType),
       StructField("b", IntegerType),
-      StructField("c", IntegerType)))
-    val fieldValues = Array(1, 2, 3)
+      StructField("c", IntegerType),
+      StructField("d", IntegerType),
+      StructField("e", IntegerType),
+      StructField("f", IntegerType)))
+    val fieldValues = Array(1, 2, 3, 4, 5, 6)
     Literal.create(create_row(fieldValues: _*), schema)
   }
 
-  test("should collapse successive DropFields into one") {
-    assertEquivalent(
+  test("should collapse successive DropFields call into a single DropFields call") {
+    assertEquivalentPlanAndEvaluation(
       DropFields(DropFields(inputStruct, "b"), "c"),
-      DropFields(inputStruct, "b", "c"))
+      DropFields(inputStruct, "b", "c"),
+      create_row(1, 4, 5, 6))
 
-    assertEquivalent(
+    assertEquivalentPlanAndEvaluation(
       DropFields(DropFields(inputStruct, "a", "b"), "c"),
-      DropFields(inputStruct, "a", "b", "c"))
+      DropFields(inputStruct, "a", "b", "c"),
+      create_row(4, 5, 6))
+
+    assertEquivalentPlanAndEvaluation(
+      DropFields(DropFields(inputStruct, "a"), "b", "c"),
+      DropFields(inputStruct, "a", "b", "c"),
+      create_row(4, 5, 6))
+
+    assertEquivalentPlanAndEvaluation(
+      DropFields(DropFields(inputStruct, "a", "b"), "c", "d"),
+      DropFields(inputStruct, "a", "b", "c", "d"),
+      create_row(5, 6))
   }
 }
