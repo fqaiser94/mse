5d6ff21c9334e525550a6e9030a64a9710026ba1
fqaiser94@gmail.com <fqaiser94@gmail.com>
2020-03-15 21:13:55 -0400

    trying to fix pyspark dependency

--- python/setup.py
+++ python/setup.py
@@ -3,14 +3,14 @@ import setuptools
 setuptools.setup(
     name='mse',
     packages=['mse'],
-    version='0.1.2',
+    version='0.1.3',
     license='Apache license 2.0',
     description='Make Structs Easy',
     author='fqaiser94',
     author_email='',
     url='https://github.com/fqaiser94/mse',
     keywords=['pyspark', 'struct', 'StructType'],
-    install_requires=['pyspark'],
+    install_requires=['pyspark>=2.0'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
